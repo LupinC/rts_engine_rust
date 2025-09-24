a774d10994d1884109f2f6ce7117bdf17cf42701
Lupin(Yuan) Cai <lupin.cai@emory.edu>
2025-09-24 17:33:13 -0500

    feature: window resizable

--- src/app/setup.rs
+++ src/app/setup.rs
@@ -8,6 +8,12 @@ impl Plugin for CoreWindowPlugin {
             primary_window: Some(Window {
                 title: "RTS Editor (Shell)".into(),
                 resolution: (1280., 800.).into(),
+                resizable: true,
+                resize_constraints: WindowResizeConstraints {
+                    min_width: 800.,
+                    min_height: 600.,
+                    ..default()
+                },
                 ..default()
             }),
             ..default()
